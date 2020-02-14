8d853d75b06bf9f713c7c7d869b3824369141c9f
Mark Clark <markjclark@outlook.com>
2020-02-14 10:53:09 -0500

    Updated to gulp4;

--- gulpfile.js
+++ gulpfile.js
@@ -4,40 +4,36 @@ var sourcemaps = require('gulp-sourcemaps');
 var nunjucksRender = require('gulp-nunjucks-render');
 var express = require('express');
 
-gulp.task('nunjucks', function() {
-    return gulp.src('app/pages/**/*.+(html|nunjucks|njk)')
-    .pipe(nunjucksRender({
-        path: ['app/templates']
-      }))
-    .pipe(gulp.dest('web'))
-  });
+gulp.task('nunjucks', () => 
+    gulp.src('app/pages/**/*.+(html|nunjucks|njk)')
+      .pipe(nunjucksRender({
+          path: ['app/templates']
+        }))
+      .pipe(gulp.dest('web'))
+  );
   
-gulp.task('deploy-artifacts', function(){
-    var deployFavicon = gulp.src('app/assets/favicon/**/*.+(png|xml|ico|json)')
-        .pipe(gulp.dest('web'));
-    var deployImages = gulp.src('app/assets/images/**/*')
-        .pipe(gulp.dest('web/images'));
-    var deployScripts = gulp.src('app/assets/scripts/**/*.+(js)')
-        .pipe(gulp.dest('web/scripts'));
-        
-    return [deployFavicon, deployImages, deployScripts];
-  });
+gulp.task('deploy-favicon', () => gulp.src('app/assets/favicon/**/*.+(png|xml|ico|json)').pipe(gulp.dest('web')));
+gulp.task('deploy-images', () => gulp.src('app/assets/images/**/*').pipe(gulp.dest('web/images')));
+gulp.task('deploy-scripts', () => gulp.src('app/assets/scripts/**/*.+(js)').pipe(gulp.dest('web/scripts')));
+gulp.task('deploy-artifacts', gulp.series('deploy-favicon', 'deploy-images', 'deploy-scripts'));
 
-gulp.task('sass', function () {
-    return gulp.src('app/assets/scss/styles.scss')
+gulp.task('sass', () =>
+    gulp.src('app/assets/scss/styles.scss')
       .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('web/css'));
-  });
+      .pipe(gulp.dest('web/css'))
+  );
 
-gulp.task('watch', function(){
-  gulp.watch('app/assets/scss/**/*.scss', ['sass']);
-  gulp.watch('app/**/*.njk', ['nunjucks']);
-  gulp.watch(['app/assets/favicon/**/*','app/assets/images/**/*','app/assets/scripts/**/*'], ['deploy-artifacts']);
+gulp.task('watch', (done) => {
+  gulp.watch('app/assets/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('app/**/*.njk', gulp.series('nunjucks'));
+  gulp.watch(['app/assets/favicon/**/*','app/assets/images/**/*','app/assets/scripts/**/*'], gulp.series('deploy-artifacts'));
+
+  return done();
 });
 
-gulp.task('serve', function(){
+gulp.task('serve', (done) => {
   const app = express();
   const port = 3030;
 
@@ -46,6 +42,8 @@ gulp.task('serve', function(){
     res.sendFile('index.html', {root : __dirname + '/web'});
   });
   app.listen(port, () => console.log(`Example app listening on port ${port}`));
+
+  return done();
 });
 
-gulp.task('default', ['sass', 'nunjucks', 'deploy-artifacts', 'watch']);
+gulp.task('default', gulp.series('sass', 'nunjucks', 'deploy-artifacts', 'watch'));
