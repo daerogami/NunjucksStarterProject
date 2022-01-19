966e413a63e1efb66e0dadf4111de488ca51c5b0
Mark Clark <mark@tusksoft.com>
2022-01-19 15:39:34 -0500

    Updating packages; Moving express to devDependencies;

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var sourcemaps = require('gulp-sourcemaps');
 var nunjucksRender = require('gulp-nunjucks-render');
 var express = require('express');
